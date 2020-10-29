d7ae75865aee75d801d3d003a080bf8b684aef1d
GeorgiyRylov <yura102@mail.ru>
2020-10-29 14:42:27 +0300

    les 1 first

---
+++ homeworks/lesson1/task1.py
@@ -0,0 +1,10 @@
+first_variable_string = "Hello"
+first_variable_int = 10
+first_variable_float = 10.33
+
+print("Переменная типа str "  first_variable_string)
+print("Переменная типа int "  first_variable_int)
+print("Переменная типа float " first_variable_float)
+
+#user_name = input('Ваше имя\n')
+#print("Hello {}".format(user_name))
---
+++ les1.py
@@ -0,0 +1,6 @@
+first_variable_string = "HEllO GB"
+first_variable_string2 = 'Hello Gb'
+print(first_variable_string)
+user_name = input('Ваше имя\n')
+print("Hello {}".format(user_name))
+if ge>18:
